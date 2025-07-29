379da23e8ef1977eec62a00e388ca6758f0b0746
rider <wordwar46@gmail.com>
2025-07-29 08:31:22 -0500

    deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === "production" ? "/Life-Syn2/" : "/",
+  base: "/Life-Syn2/", // Siempre usa la base del repo para GitHub Pages
   build: {
     outDir: "dist",
     assetsDir: "assets",
